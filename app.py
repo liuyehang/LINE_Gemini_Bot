54ba7f760056a05126c42976735871ff70efeed8
liuyehang <911189289@qq.com>
2025-05-16 20:24:20 +0800

    app.py

--- app.py
+++ app.py
@@ -124,5 +124,10 @@ def delete_history():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
+
 
